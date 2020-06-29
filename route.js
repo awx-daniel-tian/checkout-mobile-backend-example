9c679f34a6f783ff94529d8595146dbe48014faa
Daniel Tian <daniel.tian@airwallex.com>
2020-06-29 15:24:45 +0800

    refacto token client

--- route.js
+++ route.js
@@ -1,35 +1,29 @@
 const express = require("express");
-const { baseURL } = require("./config");
-const axios = require("axios");
 const router = express.Router();
+const { getToken } = require("./utils/TokenClient");
+
+const axios = require("axios");
+const { v4: uuidv4 } = require("uuid");
+
 require("dotenv").config();
+const { baseURL } = require("./config");
 
 const clientId = process.env.clientId,
   apiKey = process.env.apiKey;
 
 // serving very basic frontend
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index");
 });
 
-// testing login function
-router.get("/login", async (req, res) => {
+// create customer
+router.get("/create", async (_, res) => {
   try {
-    const {
-      data: { token },
-    } = await axios({
-      url: `${baseURL}/api/v1/authentication/login?api_key=${apiKey}&client_id=${clientId}`,
-      method: "POST",
-      timeout: 0,
-      headers: {
-        "x-client-id": clientId,
-        "x-api-key": apiKey,
-      },
-    });
+    const token = await getToken();
 
-    res.status(200).json({ token: token });
+    res.status(400).json({ token: token });
   } catch (err) {
-    res.status(400).json({ error: err });
+    res.status(400).json({ error: err.message });
   }
 });
 
---
+++ utils/TokenClient.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const { baseURL } = require("../config");
+
+const clientId = process.env.clientId,
+  apiKey = process.env.apiKey;
+
+let jsonToken;
+
+const login = async () => {
+  try {
+    const {
+      data: { token },
+    } = await axios({
+      url: `${baseURL}/api/v1/authentication/login?api_key=${apiKey}&client_id=${clientId}`,
+      method: "POST",
+      timeout: 0,
+      headers: {
+        "x-client-id": clientId,
+        "x-api-key": apiKey,
+      },
+    });
+
+    return token;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getToken = async () => {
+  if (!jsonToken) {
+    // fetch a new pair
+    jsonToken = await login();
+  } else {
+    // check if expired or not
+    const { exp } = jwt.decode(jsonToken);
+    if (Date.now() >= exp * 1000) {
+      // fetch new key
+      jsonToken = await login();
+    }
+  }
+  return jsonToken;
+};
+
+module.exports = { getToken };
